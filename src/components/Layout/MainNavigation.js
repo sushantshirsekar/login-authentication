0659e26212852d2c86c7419aff802b6a3e45a2b5
sushantshirsekar <sushantshirsekar60@gmail.com>
2023-01-27 23:16:38 +0530

    Logout button added and redirect

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const history = useHistory(); 
   const isLoggedIn = authCtx.isLoggedIn;
+  const logoutHandler = ()=> {
+    authCtx.logout()
+    history.replace('/')
+  }
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -26,7 +32,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
